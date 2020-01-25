bf9205a09dfaa31aab0c9eb5fb0241a91e986be0
Francis Lahlouh <francislahlouh123@yahoo.com>
2020-01-25 11:54:37 -0800

    add songs to homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   before_action :authenticate
   def index
-# artists = RSpotify::Artist.search('eminem') 
-# eminem = artists.first 
-  # @songs = eminem.top_tracks(:US)
+    artists = RSpotify::Artist.search('eminem') 
+    eminem = artists.first 
+    @songs = eminem.top_tracks(:US)
   end
   def authenticate 
     RSpotify.authenticate(ENV['SPOTIFY_API_CLIENT_ID'],ENV['SPOTIFY_API_CLIENT_SECRET'] )
