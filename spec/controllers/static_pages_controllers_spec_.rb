6c9b5163d139d3c567ed7a912673ec53937ee4b6
Francis Lahlouh <francislahlouh123@yahoo.com>
2020-01-25 12:28:58 -0800

    added new changes

---
+++ spec/controllers/static_pages_controllers_spec_.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe StaticPagesController, type: :controller do
+   describe "StaticPagesController#index action" do
+   it "should successfully show the page" do
+      get :index
+      expect(response).to have_http_status(:success)
+
+    end
+
+  end
+end
