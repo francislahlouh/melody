43d36138111ad74883cb81d7374aaaace0e950bf
Francis Lahlouh <francislahlouh123@yahoo.com>
2020-01-24 20:26:04 -0800

    added new changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
- def index
- end
+  before_action :authenticate
+  def index
+# artists = RSpotify::Artist.search('eminem') 
+# eminem = artists.first 
+  # @songs = eminem.top_tracks(:US)
+  end
+  def authenticate 
+    RSpotify.authenticate("14ef885e385a4454b416c7eaba1d1ba5", "2cb49284fa7645009f6fd486c53e05ef")
+  end
 end 
